fd5e425e76a89e310880466f8b4c57e750f3b493
xixixixi1230 <zhangxinyi11111111@163.com>
2024-08-28 16:08:43 +0800

    实现搜索歌曲功能

---
+++ backend/src/main/java/generator/mapper/SingerMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.Singer;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zhangxinyi
+* @description 针对表【singer】的数据库操作Mapper
+* @createDate 2024-08-28 15:09:07
+* @Entity generator.domain.Singer
+*/
+public interface SingerMapper extends BaseMapper<Singer> {
+
+}
+
+
+
+
