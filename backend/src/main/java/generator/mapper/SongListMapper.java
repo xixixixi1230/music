56581d82ad07ad994b605b6cada2490036cb6298
xixixixi1230 <zhangxinyi11111111@163.com>
2024-08-28 19:00:09 +0800

    实现获取歌单功能

---
+++ backend/src/main/java/generator/mapper/SongListMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.SongList;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zhangxinyi
+* @description 针对表【song_list】的数据库操作Mapper
+* @createDate 2024-08-28 17:08:51
+* @Entity generator.domain.SongList
+*/
+public interface SongListMapper extends BaseMapper<SongList> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/generator/service/SongListService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.SongList;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author zhangxinyi
+* @description 针对表【song_list】的数据库操作Service
+* @createDate 2024-08-28 17:08:51
+*/
+public interface SongListService extends IService<SongList> {
+
+}
