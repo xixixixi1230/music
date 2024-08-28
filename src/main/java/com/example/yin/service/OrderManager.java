6d05848457ce729561f2379f1ba14e7a4ea9f2ca
ljm11111 <109942777+ljm11111@users.noreply.github.com>
2024-08-29 00:04:26 +0800

    Delete src directory

--- src/main/java/com/example/yin/service/OrderManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.yin.service;
-
-import com.example.yin.model.domain.Order;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface OrderManager {
-    void sendCode(String code,String reciveAddress);
-}
