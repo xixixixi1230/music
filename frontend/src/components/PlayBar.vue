d2d1b3f7f25557bfbe580da947c1ea0d627c2021
Tlllqy <109964353+Tlllqy@users.noreply.github.com>
2024-08-31 19:23:45 +0800

    注册和个人信息生日接口 修改头像上传显示 头像和playbar样式

--- frontend/src/components/PlayBar.vue
+++ frontend/src/components/PlayBar.vue
@@ -14,7 +14,7 @@
       </div>
       <!-- 播放 -->
       <div class="item" @click="togglePlay">
-        <svg class="icon">
+        <svg :key="playButtonUrl" class="icon">
           <use :xlink:href="playButtonUrl"></use>
         </svg>
       </div>
@@ -26,7 +26,7 @@
       </div>
       <!-- 歌曲图片 -->
       <div class="item-img" @click="toLyric">
-        <img :src="picUrl"/>
+        <img :src="picUrl" :class="imgClass"/>
       </div>
       <!-- 播放进度 -->
       <div class="playing-speed">
@@ -96,7 +96,9 @@ export default {
       mouseStartX: 0,      //拖拽开始位置
       tag: false,             //拖拽开始结束的标志，当开始拖拽，它的值才会变成true
       volume: 50,             //音量，默认一半
-      toggle: false            //显示隐藏播放器页面
+      toggle: true ,        //显示隐藏播放器页面
+      isActive:false,         //初始未收藏
+      //isPlay:false          //初始图片不旋转
     }
   },
   computed: {
@@ -118,9 +120,31 @@ export default {
       'userId',               //当前登录用户的id
       'isActive',             //当前播放的歌曲是否已收藏
       'playerStatus'
-    ])
+    ]),
+    //添加图片随音乐播放旋转效果
+    // rotation(){
+    //   return this.isPlay ? 'rotate(360deg)' : 'rotate(0deg)';
+    // },
+    imgClass() {
+    return {
+      rotating: this.isPlay,
+      paused: !this.isPlay
+    };
+  }
   },
   watch: {
+  //   resetProgress() {
+  //   this.currentTime = 0; // 重置当前播放时间
+  //   this.$refs.audio.currentTime = 0; // 如果使用 <audio> 标签
+  //   this.$refs.progressBar.value = 0; // 如果使用 <input type="range"> 作为进度条
+  // },
+  resetProgress() {
+    this.curTime = 0;  // 重置当前时间
+    this.nowTime = this.formatSeconds(0); // 将显示的当前时间设置为 00:00
+    this.curLength = 0; // 重置进度条长度
+    this.songTime = this.formatSeconds(this.duration); // 更新歌曲的总时间
+    this.$forceUpdate(); // 强制更新视图
+  },
 
     //切换播放状态的图标
     isPlay () {
@@ -135,6 +159,14 @@ export default {
       this.songTime = this.formatSeconds(this.duration)
       this.curLength = (this.curTime / this.duration) * 100
     },
+    // 添加对 playButtonUrl 的监听
+  // playButtonUrl(newVal, oldVal) {
+  //   // 手动触发组件的重新渲染
+  //     this.$forceUpdate();
+  // },
+  playButtonUrl() {
+    return this.isPlay ? '#icon-zanting' : '#icon-bofang';
+  },
     //音量变化
     volume () {
       this.$store.commit('setVolume', this.volume / 100)
@@ -157,244 +189,252 @@ export default {
       document.querySelector('.volume').classList.remove('show-volume')
     }, false)
   },
-  methods: {
-    //是否显示播放器
-    toggleStatus () {
-      this.$store.commit("updPlayerStatus",!this.$store.getters.playerStatus)
-    },
-    //提示信息
-    notify (title, type) {
-      this.$notify({
-        title: title,
-        type: type
-      })
-    },
-    //控制音乐播放、暂停
-    togglePlay () {
-      if (this.isPlay) {
-        this.$store.commit('setIsPlay', false)
-      } else {
-        this.$store.commit('setIsPlay', true)
-      }
-    },
-    //解析时间
-    formatSeconds (value) {
-      let theTime = parseInt(value)
-      let result = ''    //返回值
-      let hour = parseInt(theTime / 3600)    //小时
-      let minute = parseInt((theTime / 60) % 60) //分钟
-      let second = parseInt(theTime % 60)        //秒
-      if (hour > 0) {
-        if (hour < 10) {
-          result = '0' + hour + ':'
-        } else {
-          result = hour + ':'
-        }
-      }
-      if (minute > 0) {
-        if (minute < 10) {
-          result += '0' + minute + ':'
-        } else {
-          result += minute + ':'
-        }
-      } else {
-        result += '00:'
-      }
-      if (second > 0) {
-        if (second < 10) {
-          result += '0' + second
-        } else {
-          result += second
-        }
-      } else {
-        result += '00'
-      }
-      return result
-    },
-    //拖拽开始
-    mousedown (e) {
+
+methods: {
+  // 是否显示播放器
+  toggleStatus () {
+    this.$store.commit("updPlayerStatus", !this.$store.getters.playerStatus)
+  },
+  // 提示信息
+  notify (title, type) {
+    this.$notify({
+      title: title,
+      type: type
+    })
+  },
+  // 控制音乐播放、暂停
+  togglePlay () {
+    if (this.isPlay) {
+      this.$store.commit('setIsPlay', false)
+    } else {
+      this.$store.commit('setIsPlay', true)
+    }
+  },
+  // 解析时间
+  formatSeconds (value) {
+    let theTime = parseInt(value)
+    let result = ''
+    let hour = parseInt(theTime / 3600)
+    let minute = parseInt((theTime / 60) % 60)
+    let second = parseInt(theTime % 60)
+    if (hour > 0) {
+      result = (hour < 10 ? '0' : '') + hour + ':'
+    }
+    result += (minute < 10 ? '0' : '') + minute + ':'
+    result += (second < 10 ? '0' : '') + second
+    return result
+  },
+  // 拖拽开始
+  mousedown (e) {
+    this.mouseStartX = e.clientX
+    this.tag = true
+  },
+  // 拖拽结束
+  mouseup () {
+    this.tag = false
+  },
+  // 拖拽中
+  mousemove (e) {
+    if (!this.duration) return
+    if (this.tag) {
+      let movementX = e.clientX - this.mouseStartX
+      let curLength = this.$refs.curProgress.getBoundingClientRect().width
+      let newPercent = ((movementX + curLength) / this.progressLength) * 100
+      if (newPercent > 100) newPercent = 100
+      this.curLength = newPercent
       this.mouseStartX = e.clientX
-      this.tag = true
-    },
-    //拖拽结束
-    mouseup () {
-      this.tag = false
-    },
-    //拖拽中
-    mousemove (e) {
-      if (!this.duration) {
-        return false
+      this.changeTime(newPercent)
+    }
+  },
+  // 更改歌曲进度
+  changeTime (percent) {
+    let newCurTime = (percent * 0.01) * this.duration
+    this.$store.commit('setChangeTime', newCurTime)
+  },
+  // 点击播放条切换播放进度
+  updatemove (e) {
+    if (!this.tag) {
+      let newPercent = (e.offsetX / this.progressLength) * 100
+      if (newPercent > 100) newPercent = 100
+      else if (newPercent < 0) newPercent = 0
+      this.curLength = newPercent
+      this.changeTime(newPercent)
+    }
+  },
+  // 显示播放中的歌曲列表
+  changeAside () {
+    this.$store.commit('setShowAside', true)
+  },
+  // 上一首
+  prev () {
+    if (this.listIndex !== -1 && this.listOfSongs.length > 1) {
+      if (this.listIndex > 0) {
+        this.$store.commit('setListIndex', this.listIndex - 1)
+      } else {
+        this.$store.commit('setListIndex', this.listOfSongs.length - 1)
       }
-      if (this.tag) {
-        let movementX = e.clientX - this.mouseStartX       //点点移动的距离
-        let curLength = this.$refs.curProgress.getBoundingClientRect().width
-        let newPercent = ((movementX + curLength) / this.progressLength) * 100
-        if (newPercent > 100) {
-          newPercent = 100
-        }
-        this.curLength = newPercent
-        this.mouseStartX = e.clientX
-        this.changeTime(newPercent)
+      this.toplay(this.listOfSongs[this.listIndex].url)
+      this.resetProgress()
+      this.togglePlay() // 确保播放状态被设置为播放
+    }
+  },
+  // 下一首
+  next () {
+    if (this.listIndex !== -1 && this.listOfSongs.length > 1) {
+      if (this.listIndex < this.listOfSongs.length - 1) {
+        this.$store.commit('setListIndex', this.listIndex + 1)
+      } else {
+        this.$store.commit('setListIndex', 0)
       }
-    },
-    //更改歌曲进度
-    changeTime (percent) {
-      let newCurTime = (percent * 0.01) * this.duration
-      this.$store.commit('setChangeTime', newCurTime)
-    },
-    //点击播放条切换播放进度
-    updatemove (e) {
-      if (!this.tag) {
-        //进度条的左侧坐标
-        let curLength = this.$refs.bg.offsetLeft
-        let newPercent = ((e.clientX - curLength) / this.progressLength) * 100
-        if (newPercent > 100) {
-          newPercent = 100
-        } else if (newPercent < 0) {
-          newPercent = 0
-        }
-        this.curLength = newPercent
-        this.changeTime(newPercent)
-      }
-    },
-    //显示播放中的歌曲列表
-    changeAside () {
-      this.$store.commit('setShowAside', true)
-    },
-    //上一首
-    prev () {
-      if (this.listIndex != -1 && this.listOfSongs.length > 1) {    //当前处于不可能状态或者只有只有一首音乐的时候不执行）
-        if (this.listIndex > 0) {                                 //不是第一首音乐
-          this.$store.commit('setListIndex', this.listIndex - 1)  //直接返回上一首
-        } else {                                                  //当前是第一首音乐
-          this.$store.commit('setListIndex', this.listOfSongs.length - 1)  //切换到倒数第一首
-        }
-        this.toplay(this.listOfSongs[this.listIndex].url)
-      }
-    },
-    //下一首
-    next () {
-      if (this.listIndex != -1 && this.listOfSongs.length > 1) {    //当前处于不可能状态或者只有只有一首音乐的时候不执行）
-        if (this.listIndex < this.listOfSongs.length - 1) {                                 //不是最后一首音乐
-          this.$store.commit('setListIndex', this.listIndex + 1)  //直接返回下一首
-        } else {                                                      //当前是最后一首音乐
-          this.$store.commit('setListIndex', 0)  //切换到第一首
-        }
-        this.toplay(this.listOfSongs[this.listIndex].url)
-      }
-    },
-    //播放音乐
-    toplay: function (url) {
-      if (url && url != this.url) {
-        this.$store.commit('setId', this.listOfSongs[this.listIndex].id)
-        this.$store.commit('setUrl', this.$store.state.configure.HOST + url)
-        this.$store.commit('setPicUrl', this.$store.state.configure.HOST + this.listOfSongs[this.listIndex].pic)
-        this.$store.commit('setTitle', this.replaceFName(this.listOfSongs[this.listIndex].name))
-        this.$store.commit('setArtist', this.replaceLName(this.listOfSongs[this.listIndex].name))
-        this.$store.commit('setLyric', this.parseLyric(this.listOfSongs[this.listIndex].lyric))
-        this.$store.commit('setIsActive', false)
-        if (this.loginIn) {
-          getCollectOfUserId(this.userId)
-            .then(res => {
-              for (let item of res) {
-                if (item.songId == id) {
-                  this.$store.commit('setIsActive', true)
-                  break
-                }
-              }
-            })
-        }
-      }
-    },
-    //获取名字前半部分--歌手名
-    replaceLName (str) {
-      let arr = str.split('-')
-      return arr[0]
-    },
-    //获取名字后半部分--歌名
-    replaceFName (str) {
-      let arr = str.split('-')
-      return arr[1]
-    },
-    //解析歌词
-    parseLyric (text) {
-      let lines = text.split('\n')                   //将歌词按行分解成数组
-      let pattern = /\[\d{2}:\d{2}.(\d{3}|\d{2})\]/g //时间格式的正则表达式
-      let result = []                                //返回值
-      //对于歌词格式不对的直接返回
-      if (!(/\[.+\]/.test(text))) {
-        return [[0, text]]
-      }
-      //去掉前面格式不正确的行
-      while (!pattern.test(lines[0])) {
-        lines = lines.slice(1)
-      }
-      //遍历每一行，形成一个每行带着俩元素的数组，第一个元素是以秒为计算单位的时间，第二个元素是歌词
-      for (let item of lines) {
-        let time = item.match(pattern)  //存前面的时间段
-        let value = item.replace(pattern, '')//存后面的歌词
-        for (let item1 of time) {
-          let t = item1.slice(1, -1).split(':')   //取出时间，换算成数组
-          if (value != '') {
-            result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value])
-          }
-        }
-      }
-      //按照第一个元素--时间--排序
-      result.sort(function (a, b) {
-        return a[0] - b[0]
-      })
-      return result
-    },
-    //转向歌词页面
-    toLyric () {
-      this.$router.push({path: `/lyric`})
-    },
-    //下载音乐
-    download () {
-      download(this.url)
-        .then(res => {
-          let content = res.data
-          let eleLink = document.createElement('a')
-          eleLink.download = `${this.artist}-${this.title}.mp3`
-          eleLink.style.display = 'none'
-          //把字符内容转换成blob地址
-          let blob = new Blob([content])
-          eleLink.href = URL.createObjectURL(blob)
-          //把链接地址加到document里
-          document.body.appendChild(eleLink)
-          //触发点击
-          eleLink.click()
-          //然后移除掉这个新加的控件
-          document.body.removeChild(eleLink)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    },
-    //收藏
-    collection () {
+      this.toplay(this.listOfSongs[this.listIndex].url)
+      this.resetProgress()
+      this.togglePlay() // 确保播放状态被设置为播放
+    }
+  },
+
+
+  // 播放音乐
+  toplay (url) {
+    if (url && url !== this.url) {
+      this.$store.commit('setId', this.listOfSongs[this.listIndex].id)
+      this.$store.commit('setUrl', this.$store.state.configure.HOST + url)
+      this.$store.commit('setPicUrl', this.$store.state.configure.HOST + this.listOfSongs[this.listIndex].pic)
+      this.$store.commit('setTitle', this.replaceFName(this.listOfSongs[this.listIndex].name))
+      this.$store.commit('setArtist', this.replaceLName(this.listOfSongs[this.listIndex].name))
+      this.$store.commit('setLyric', this.parseLyric(this.listOfSongs[this.listIndex].lyric))
+      this.$store.commit('setIsActive', false)
       if (this.loginIn) {
-        var params = new URLSearchParams()
-        params.append('userId', this.userId)
-        params.append('type', 0)
-        params.append('songId', this.id)
-        setCollect(params)
+        getCollectOfUserId(this.userId)
           .then(res => {
-            if (res.code === 1) {
-              this.$store.commit('setIsActive', true)
-              this.notify('收藏成功', 'success')
-            } else if (res.code === 2) {
-              this.notify('已收藏', 'warning')
-            } else {
-              this.notify('收藏失败', 'error')
+            for (let item of res) {
+              if (item.songId == this.id) {
+                this.$store.commit('setIsActive', true)
+                break
+              }
             }
           })
-      } else {
-        this.notify('请先登录', 'warning')
       }
     }
+  },
+  // // 获取名字前半部分--歌手名
+  // replaceLName (str) {
+  //   let arr = str.split('-')
+  //   return arr[0]
+  // },
+  // // 获取名字后半部分--歌名
+  // replaceFName (str) {
+  //   let arr = str.split('-')
+  //   return arr[1]
+  // },
+  // 解析歌词
+  parseLyric (text) {
+    let lines = text.split('\n')
+    let pattern = /\[\d{2}:\d{2}.(\d{3}|\d{2})\]/g
+    let result = []
+    if (!(/\[.+\]/.test(text))) return [[0, text]]
+    while (!pattern.test(lines[0])) lines = lines.slice(1)
+    for (let item of lines) {
+      let time = item.match(pattern)
+      let value = item.replace(pattern, '')
+      for (let item1 of time) {
+        let t = item1.slice(1, -1).split(':')
+        if (value !== '') result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value])
+      }
+    }
+    result.sort((a, b) => a[0] - b[0])
+    return result
+  },
+
+
+  // 页面
+  toLyric() {
+    if (this.$route.path === '/lyric') {
+      this.$router.go(-1);
+    } else {
+      this.$router.push({ path: '/lyric' });
+    }
+  },
+  // 下载音乐
+
+
+  download () {
+    download(this.url)
+      .then(res => {
+        let content = res.data
+        let eleLink = document.createElement('a')
+        eleLink.download = `${this.artist}-${this.title}.mp3`
+        eleLink.style.display = 'none'
+        let blob = new Blob([content])
+        eleLink.href = URL.createObjectURL(blob)
+        document.body.appendChild(eleLink)
+        eleLink.click()
+        document.body.removeChild(eleLink)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },
+  // 收藏
+  collection() {
+      if (this.loginIn) {
+        // const params = new URLSearchParams();
+        // params.append('userId', this.userId);
+        // params.append('type', 0);
+        // params.append('songId', this.id);
+        let params={
+          'userId': this.userId,
+          'type': this.type,
+          'songId':this.songId,
+        }
+        setCollect(params)
+          .then(res => {
+            if (res.code === 200) {
+              this.isActive = !this.isActive; // 切换图标状态
+              this.$store.commit('setIsActive', this.isActive); // 更新 Vuex 状态
+              this.notify('收藏成功', 'success');
+            } else if (res.code === 2) {
+              this.notify('已收藏', 'warning');
+            }
+          })
+          .catch(() => {
+            this.notify('操作失败', 'error');
+          });
+      } else {
+        this.notify('请登录后再收藏', 'warning');
+      }
+    },
+
+  // collection () {
+  //   if (this.loginIn) {
+  //     var params = new URLSearchParams()
+  //     params.append('userId', this.userId)
+  //     params.append('type', 0)
+  //     params.append('songId', this.id)
+  //     setCollect(params)
+  //       .then(res => {
+  //         if (res.code === 1) {
+  //           this.$store.commit('setIsActive', true)
+  //           this.notify('收藏成功', 'success')
+  //         } else if (res.code === 2) {
+  //           this.notify('已收藏', 'warning')
+  //         }
+  //       })
+  //   } else {
+  //     this.notify('请登录后再收藏', 'warning')
+  //   }
+  // },
+  // 重置播放进度和时间
+  resetProgress() {
+    this.nowTime = '00:00'
+    this.songTime = this.formatSeconds(this.duration)
+    this.curLength = 0
+    this.$store.commit('setChangeTime', 0)
   }
+}
+
+
+
+
 }
 </script>
 <style lang="scss" scoped>
--- frontend/src/components/Upload.vue
+++ frontend/src/components/Upload.vue
@@ -1,105 +1,107 @@
 <template>
-    <div class="upload">
-        <p class="title">修改头像</p>
-        <hr/>
-        <div class="section">
-            <el-upload
-                drag
-                :action="uploadAction"
-                :show-file-list="false"
-                :on-success="handleAvatorSuccess"
-                :before-upload="beforeAvatorUpload"
-                >
-                <i class="el-icon-upload"></i>
-                <div>
-                    将文件拖到此处，或<span style="color:blue">修改头像</span>
-                </div>
-                <div slot="tip" style="margin-left: 50px;margin-top:10px;">只能上传jpg/png文件，且不能超过10MB</div>
-            </el-upload>
-            <!-- 预览当前头像 -->
-            <img :src="avator" alt="用户头像" v-if="avator" class="preview-img" style="margin-top: 20px;"/>
-            <el-button @click="uploadFile" style="margin-top: 5px;">Upload</el-button>
-        </div>
+  <div class="upload">
+    <p class="title">修改头像</p>
+    <hr />
+    <div class="section">
+      <el-upload action="http://localhost:8080/" list-type="picture-card" :auto-upload="false" :on-change="handleChange"
+        :show-file-list="false">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <div v-if="imageUrl">
+        <img :src="imageUrl" alt="Uploaded Image" style="width: 100px; height: 100px;" />
+      </div>
+      <button @click="uploadFile">上传</button>
     </div>
+  </div>
 </template>
 <script>
-import {mapGetters} from 'vuex'
-import {mixin} from '../mixins'
-import {updateUserAvator} from '../api/index'
+import { mapGetters } from "vuex";
+import { mixin } from "../mixins";
+import { updateUserAvator } from "../api/index";
+import axios from 'axios';
+
 
 export default {
-    name: 'upload',
-    props: {
-      uploadAction: {
-        type: String,
-        required: true
+  name: "upload",
+  data() {
+    return {
+      imageUrl: "",
+      selectedFile: null
+    };
+  },
+  mixins: [mixin],
+  computed: {
+    ...mapGetters(["userId", "avator"])
+  },
+  mounted() {
+    console.log(this.userId);
+    console.log(this.avator);
+  },
+  methods: {
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
       }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
     },
-    data() {
-      return {
-        selectedFile: null, // 存储用户选择的文件
-      };
-     },
-    mixins: [mixin],
-    computed:{
-        ...mapGetters([
-            'userId',
-            'avator',
-        ])
+    handleChange(file) {
+      // 保存文件和生成本地预览 URL
+      this.selectedFile = file.raw;
+      this.imageUrl = URL.createObjectURL(file.raw);
     },
-    methods:{
-        uploadFile(){
-          if (!this.selectedFile) {
-            // alert('Please select a file to upload.');
-            return;
+    uploadFile() {
+      if (!this.selectedFile) {
+        //this.$message.error("请先选择文件！");
+        return;
+      }
+      console.log("upload");
+      //   创建 FormData 对象
+      const formData = new FormData();
+      formData.append("file", this.selectedFile);
+      console.log(formData);
+      console.log(this.userId, this.selectedFile);
+
+      let params = {
+        "file": this.selectedFile,
+        "id": this.userId
+      }
+
+      let id = this.userId
+
+      axios
+        .post(`http://localhost:8080/user/avatar/update?id=${id}`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
           }
-          // 创建一个FormData对象并附加文件数据
-          const formData = new FormData();
-          formData.append('file', this.selectedFile); // "file"是请求的参数名
-          let params={
-            id:this.userId,
-            file:formData
-          }
-          updateUserAvator(params)
-            .then(res =>{
-              console.log(res);
-                if(res.code == 200){
-                    this.notify('修改成功','success');
-                }else{
-                    this.notify('修改失败','error');
-                }
-            })
-            .catch(err =>{
-                console.log(err);
-            })
-        },
-         //上传成功
-         handleAvatorSuccess(res, file) {
-            // 在上传成功后直接设置 Vuex 中的 avator 状态
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                this.$store.commit('setAvator', e.target.result); // 使用 Base64 数据设置头像
-                this.notify('修改成功', 'success');
-            };
-            reader.readAsDataURL(file.raw); // 读取上传的文件为 Base64
-        },
-        //上传之前的校验
-        beforeAvatorUpload(file){
-            const isJPG = file.type=='image/jpeg';
-            const isLt10M = file.size /1024/1024<10;
-            if(!isJPG){
-                this.notify('上传头像图片只能是JPG格式','error');
-                return false;
-            }
-            if(!isLt10M){
-                this.notify('上传头像图片不能大于10MB','error');
-                return false;
-            }
-            return true;
-        }
+        })
+        .then(res => {
+          //console.log(res);
+          const newAvatarUrl = res.data.data;
+          // 确认 URL 是否正确
+          console.log("New Avatar URL: ", newAvatarUrl);
+          // 更新 Vuex Store 中的 avator 状态
+          this.$store.commit('setAvator', newAvatarUrl);
+
+          // 强制组件重新渲染
+          this.$forceUpdate();
+          console.log("File uploaded successfully");
+        })
+        .catch(error => {
+          console.error("Error uploading file:", error);
+        });
+
     }
-}
+
+  }
+  
+};
 </script>
 <style lang="scss" scoped>
-@import '../assets/css/upload.scss';
+@import "../assets/css/upload.scss";
 </style>
